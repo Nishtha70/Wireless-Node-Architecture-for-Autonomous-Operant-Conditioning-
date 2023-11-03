cf3b5d5263ac5a3fcf81f6ed2ea4c4daa4960c05
Nishtha70 <56824935+Nishtha70@users.noreply.github.com>
2023-11-03 19:20:56 +0100

    Update pre_training_client.py
    Added comments

--- Client-system/pre_training_client.py
+++ Client-system/pre_training_client.py
@@ -1,21 +1,20 @@
-#import the libraries
-import cv2
-import numpy
-import torch
-import imutils
+import os
 import sys
 import time
-import yaml
-import os
 import json
 import warnings
-from datetime import date
-import csv
-import pandas as pd
-from twilio.rest import Client
-from datetime import datetime
 import argparse
 import random
+from datetime import date, datetime
+import yaml
+import csv
+import pyautogui
+
+import cv2
+import imutils
+import pandas as pd
+import torch
+from twilio.rest import Client
 
 # Define and parse command-line arguments using argparse
 parser = argparse.ArgumentParser(description="Fish Conditioning Experiment Parameters")
@@ -66,10 +65,6 @@ start_x2 = args.start_x2
 start_y1 = args.start_y1
 start_y2 = args.start_y2
 
-
-
-
-
 #Twilio SMS setup
 account_sid = 'AC174a0802c2e63396a3807e4d26b6bc88'
 auth_token = '96cb5a74da1a5d243094e45ce2ef09e2'
@@ -112,7 +107,9 @@ motor_right = [motor2A, motor2B, motor2E]
 
 
 class Cichlids_preTrain_Exp:
-    def __init__(self, tank_ID, max_pretraining_days, max_sessions_per_day, stimulus_display_time, success_threshold, max_session_duration, session_interval, max_sessions_pretraining, max_trial_per_session, display_neutral_after_time, stimulus_image, neutral_image, ROC1_x1, ROC1_y1, ROC1_x2, ROC1_y2, start_x1, start_y1, start_x2, start_y2):
+    def __init__(self, tank_ID, max_pretraining_days, max_sessions_per_day, success_threshold, unfit_threshold,
+             max_session_duration, session_interval, max_trial_per_session, display_neutral_after_time,
+             stimulus_image, neutral_image, ROC1_x1, ROC1_y1, ROC1_x2, ROC1_y2, start_x1, start_y1, start_x2, start_y2):
         self.tank_ID = tank_ID
         self.max_pretraining_days = max_pretraining_days
         self.max_sessions_per_day = max_sessions_per_day
@@ -136,17 +133,14 @@ class Cichlids_preTrain_Exp:
         self.start_y2 = start_y2
         self.coordinates_data = []
         self.pre_train_info = []
-       
         self.pretrained = False
         
-        
+    #Method to detect and track fish   
     def track_fish(self, session, video_recording):
         cx = None
         cy = None
         
-         
         #Check if frame is received by the RasPy
-
         if not cap.isOpened():
             message = client.messages.create(body="Cannot open camera", from_='+19408182187', to='+918529691031')
             print("Cannot open camera")
@@ -157,7 +151,6 @@ class Cichlids_preTrain_Exp:
             print("Cant receive frame end")
             exit()
         
-        
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
         results = model(frame, size=720)
@@ -167,7 +160,8 @@ class Cichlids_preTrain_Exp:
         results.render()
         output_image = results.ims[0] #output image after rendering
         output_image = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
-        
+
+        #calculate centroid
         data = json.loads(json_results)
         for objects in data:
             xmin = objects["xmin"]
@@ -178,47 +172,44 @@ class Cichlids_preTrain_Exp:
             cx = int((xmin+xmax)/2.0)
             cy = int((ymin+ymax)/2.0)
      
-            
         cv2.circle(output_image, (cx, cy), 2, (0,0,255), 2, cv2.FILLED)
         cv2.putText(output_image, str(str(cx)+ "," +str(cy)), (int(cx)-40, int(cy)+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
-        cv2.putText(output_image, str("Session:"+str(session)+"TS:"+str(timestamp)), (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
-            
-            
+        cv2.putText(output_image, str("Session:"+str(session)+"TS:"+str(timestamp)), (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)   
         output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
         return output_image, cx, cy
     
-    
+    #Checks if fish is present in the region of interest
     def is_fish_in_ROI(self, fish_x, fish_y, ROC1_x1, ROC1_y1, ROC1_x2, ROC1_y2):
         if ROC1_x1 <= fish_x<= ROC1_x2 and ROC1_y1 <= fish_y<= ROC1_y2:
             return True
         else:
             return False
-    
+            
+    #Displays stimulus on the screens
     def displayn(self, image_path, screen_name,  width, height, window_position):
         window_pos = [width, height]
-        
         image = cv2.imread(image_path)
         cv2.namedWindow(screen_name, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(screen_name, width, height)
         cv2.moveWindow(screen_name, window_position[0], window_position[1])
         cv2.imshow(screen_name, image)
         cv2.waitKey(2)
-        
+
+    #Saves tracked coordinates of fish in a csv
     def save_coordinates_csv(self, csv_filename):
-        
         with open(csv_filename, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Session Number", "Timestamp", "Fish_X", "Fish_Y"])
             writer.writerows(self.coordinates_data)
             
+    #Saves information about the pre-training routine
     def save_pretrain_info(self, csv_filename):
-        
         with open(csv_filename, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Session Number", "Number of Trials Performed", "Learning_Success"])
             writer.writerows(self.coordinates_data)
-            
-            
+
+    #Set up GPIO pins for motor control   
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(motor1A,GPIO.OUT)  # All pins as Outputs
@@ -228,7 +219,8 @@ class Cichlids_preTrain_Exp:
         GPIO.setup(motor2A,GPIO.OUT)  # All pins as Outputs
         GPIO.setup(motor2B,GPIO.OUT)
         GPIO.setup(motor2E,GPIO.OUT)
-        
+
+    #Operates the motor
     def motor_run(self, motor_name):
         # Going forwards
         GPIO.output(motor_name[0],GPIO.HIGH)
@@ -243,13 +235,15 @@ class Cichlids_preTrain_Exp:
         sleep(5)
         # Stop
         GPIO.output(motor_name[2],GPIO.LOW)
-        
+
+    #Randomly select a fish feeder to drop the food
     def food_drop(self, motorX, motorY):
         motor_listed = [motorX, motorY]
         food_motor = random.choice(motor_listed)
         print(food_motor)
         motor_run(food_motor)
-        
+
+    #Execute a pre-training session
     def run_session(self, session, save_recording, save_coordinates):
         session+=1
         print("start")
@@ -258,11 +252,8 @@ class Cichlids_preTrain_Exp:
         food_drop_count = 0
         session_start_time = time.time()
         
-        
         while trial_count <= self.max_trial_per_session:
-            
             while (time.time() - session_start_time) <= self.max_session_duration:
-                
                 output_image, fish_x, fish_y = self.track_fish(session, save_recording)
                 print(fish_x, fish_y)
                 cv2.imshow("frame", output_image)
@@ -282,7 +273,8 @@ class Cichlids_preTrain_Exp:
                         trial_count+=1
                         trigger_count+=1
                         food_drop_count = 0
-                        
+
+                #Fish is in Region of choice
                 if self.is_fish_in_ROI(fish_x, fish_y, self.ROC1_x1, self.ROC1_y1, self.ROC1_x2, self.ROC1_y2) == True:
                     trigger_count=0
                     fish_entry_ROC = time.time()
@@ -290,14 +282,12 @@ class Cichlids_preTrain_Exp:
                     if time.time() - fish_entry_ROC > self.display_neutral_after_time:
                         self.displayn(self.neutral_image, "Left_Screen", int(window_width/2), window_height, left_window_pos)
                         self.displayn(self.neutral_image, "Right_Screen", int(window_width/2), window_height, right_window_pos)
-
                         if food_drop_count<=0:
                             self.food_drop(motor_left, motor_right)
                             food_drop_count+=1
                     else:
                         self.displayn(self.stimulus_image, "Left_Screen", int(window_width/2), window_height, left_window_pos)
                         self.displayn(self.stimulus_image, "Right_Screen", int(window_width/2), window_height, right_window_pos)
-            print("90")
             break
             
         print("Time:",time.time() - session_start_time)
@@ -307,25 +297,17 @@ class Cichlids_preTrain_Exp:
         print(learning_success)
         return trial_count, learning_success
         
-    def run_pretrain_exp(self):
-           
+    def run_pretrain_exp(self):    
         session = 0
         consecutive_successful_sessions = 0
         consecutive_unfit_sessions = 0
         
-        
         for day in range(self.max_pretraining_days):
-            
             for session in range(self.max_sessions_per_day):
-                
                 date_session = date.today()
-                print('****')
-                save_recording = "P" + "_" + str(self.tank_ID)  + "_" + str(date_session) + "NIS"+ ".avi"
-                
+                save_recording = "P" + "_" + str(self.tank_ID)  + "_" + str(date_session) + ".avi"
                 save_coordinates = "P" + "_" + str(self.tank_ID)  +  "_" + str(date_session) + "_coords.csv"
-                
                 save_pretraining_details = "P" + "_" + str(self.tank_ID) + "_pretrain_info.csv"
-                
                 
                 output_image, fish_x, fish_y = self.track_fish(session, save_recording)
                 print(fish_x, fish_y)
@@ -335,10 +317,8 @@ class Cichlids_preTrain_Exp:
                 self.coordinates_data.append([session, session_time, fish_x, fish_y])
                 print(self.coordinates_data)
                 self.save_coordinates_csv(save_coordinates)
-                
-                
+            
                 if self.is_fish_in_ROI(fish_x, fish_y, self.start_x1, self.start_y1, self.start_x2, self.start_y2) == True:
-                    
                     learning_success, trial_count = self.run_session(session, save_recording, save_coordinates)
                     print("Session Ended")
                     cv2.waitKey(self.session_interval)
@@ -357,7 +337,7 @@ class Cichlids_preTrain_Exp:
                         
                     if consecutive_unfit_sessions >= 2:
                         print("Fish_unfit_for_training")
-                        message = client.messages.create(body="Fish unfit for training. The system is in sleep mode. Re-execute the file", from_='+19408182187', to='+918529691031')
+                        message = client.messages.create(body="Fish unfit for training. The system is in sleep mode. Re-execute the file", from_='+19408182187', to='+91852ABCD031')
                         time.sleep(0)
                         
                     if consecutive_successful_sessions >= 2:
@@ -366,17 +346,14 @@ class Cichlids_preTrain_Exp:
                             os.rename("video_record.avi", save_recording)
                         except OSError as e:
                             print("Error Renaming the file")
-                        print("Fish is pre-trained!")
-                        
+                        print("Fish is pre-trained!")  
                     else:
                         print("Fish not trained yet")
-                        continue
-                        
+                        continue      
                 else:
                     continue
      
            
-                
 experiment = Cichlids_preTrain_Exp(
     tank_ID=tank_ID,
     max_pretraining_days=max_pretraining_days,
