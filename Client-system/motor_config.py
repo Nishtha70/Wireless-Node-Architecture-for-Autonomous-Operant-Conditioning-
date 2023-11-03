d93b42e9656e51ebf540281e7b4fdc41825ae23b
Nishtha70 <56824935+Nishtha70@users.noreply.github.com>
2023-11-03 22:43:18 +0100

    Add files via upload

---
+++ Client-system/motor_config.py
@@ -0,0 +1,14 @@
+# motor_config.py
+
+# Pins for Motor Driver Inputs
+motorLA = 24
+motorLB = 23
+motorLE = 25
+
+motorRA = 20
+motorRB = 21
+motorRE = 16
+
+# Motor names
+motor_left = [motorLA, motorLB, motorLE]
+motor_right = [motorRA, motorRB, motorRE]
