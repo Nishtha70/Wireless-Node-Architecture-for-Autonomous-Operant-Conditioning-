0315512bbbe5a366ec2fe331771f8dc7ad33c4be
Nishtha70 <56824935+Nishtha70@users.noreply.github.com>
2023-11-03 19:46:53 +0100

    Update and rename Training_standalone.py to training_standalone.py
    Added comments

--- stand_alone_system/training_standalone.py
+++ stand_alone_system/training_standalone.py
@@ -1,21 +1,22 @@
-#import the libraries
-import cv2
-import numpy
-import torch
-import imutils
+import os
 import sys
 import time
-import yaml
-import os
 import json
 import warnings
+import argparse
 import random
-from datetime import date
+from datetime import date, datetime
+import yaml
 import csv
+import pyautogui
+
+import cv2
+import imutils
 import pandas as pd
+import torch
 from twilio.rest import Client
-from datetime import datetime
 from Pre_training_standalone import Cichlids_preTrain_Exp
+
 exp_instance = Cichlids_preTrain_Exp(tank_ID=1,
                                      max_pretraining_days=2,
                                      max_sessions_per_day=4,
@@ -42,42 +43,30 @@ account_sid = 'AC174a0802c2e63396a3807e4d26b6bc88'
 auth_token = '96cb5a74da1a5d243094e45ce2ef09e2'
 client = Client(account_sid, auth_token)
 
-#Enter path of neutral and stimulus display image
-stimulus_display = "/home/pi/Downloads/red_wallpaper"
-neutral_display = "/home/pi/Downloads/green_wallpaper"
-
 # Set up the screen resolution and calculate the window sizes and positions
-screen_width = 3840
-screen_height = 1080
+screen_width, screen_height = pyautogui.size()
 window_width = screen_width // 2
 window_height = screen_height
 left_window_pos = (0, 0)
 right_window_pos = (window_width, 0)
 
-model = torch.hub.load('/home/pi/yolov5', 'custom', path='/home/pi/yolov5/runs/exp_yolov5n/weights/best.pt', force_reload=True, source='local')
+# Define paths relative to the current script or project root
+repo_dir = os.path.dirname(os.path.abspath(__file__))
+weights_path = os.path.join(repo_dir, 'weights', 'best.pt')
 
+# Load the YOLOv5 model from the local directory
+model = torch.hub.load(repo_dir, 'custom', path=weights_path, force_reload=True, source='local')
+
+# Open the default camera (Camera 0), set video encoding parameters, and create a video writer
 cap = cv2.VideoCapture(0)
-
 fourcc_codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 fps = 20.0
 capture_size = (int(cap.get(3)), int(cap.get(4)))
 video_writer = cv2.VideoWriter("video_record.avi", fourcc_codec, fps, capture_size)
 
-# Pins for Motor Driver Inputs
-#Left Motor
-motorLA = 24
-motorLB = 23
-motorLE = 25
-
-#Right Motor
-motorRA = 20
-motorRB = 21
-motorRE = 16
-
-#Motor names
-motor_left = [motorLA, motorLB, motorLE]
-motor_right = [motorRA, motorRB, motorRE]
-
+# Access the feeder's motor pins and names from the motor_config module
+motor_left = motor_config.left_motor_pins
+motor_right = motor_config.right_motor_pins
 
 class Cichlids_Train_Exp:
     def __init__(self, tank_ID, max_training_days, max_sessions_per_day,  success_threshold, unfit_threshold, max_session_duration, session_interval, max_trial_per_session, display_neutral_after_time, correct_stimulus, incorrect_stimulus, neutral_image, start_x1, start_x2, start_y1, start_y2, ROC_x1, ROC_x2, ROC_y1, ROC_y2, left_ROI_x1, left_ROI_x2, left_ROI_y1, left_ROI_y2, right_ROI_x1, right_ROI_x2, right_ROI_y1, right_ROI_y2):
@@ -113,15 +102,14 @@ class Cichlids_Train_Exp:
         self.train_info = []
         self.trained = False
         
-        
+    #Save training routine information    
     def save_train_info(self, csv_filename):
-        
         with open(csv_filename, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Session Number", "Number of Trials Performed", "Response Rate", "Learning_Success"])
             writer.writerows(self.coordinates_data)
-            
-            
+
+    #Set up GPIO pins for motor control   
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         #Left Motor
@@ -133,13 +121,13 @@ class Cichlids_Train_Exp:
         GPIO.setup(motorRA,GPIO.OUT)  # All pins as Outputs
         GPIO.setup(motorRB,GPIO.OUT)
         GPIO.setup(motorRE,GPIO.OUT)
-        
+
+    #Operates the motor to drop food
     def food_drop(self, motor_name):
         # Going forward
         GPIO.output(motor_name[0],GPIO.HIGH)
         GPIO.output(motor_name[1],GPIO.LOW)
         GPIO.output(motor_name[2],GPIO.HIGH)
-        print(2)
         sleep(5)
         # Going backwards
         GPIO.output(motor_name[0],GPIO.LOW)
@@ -149,9 +137,8 @@ class Cichlids_Train_Exp:
         # Stop
         GPIO.output(motor_name[2],GPIO.LOW)
         
-        
+    #Executes a training session    
     def run_session(self, session, save_recording, save_coordinates):
-        
         session+=1
         print("start")
         trial_count = 0
@@ -163,27 +150,20 @@ class Cichlids_Train_Exp:
         response_rate = 0
         session_start_time = time.time()
         
-        
         while trial_count < self.max_trial_per_session:
-            
-            
             while (time.time() - session_start_time) <self.max_session_duration:
-                
-             
                 output_image, fish_x, fish_y = exp_instance.track_fish(session, save_recording)
-                #video_writer.write(output_image)
-                #print(time.time() - session_start_time)
-               
+                #Saves the recording and tracked coordinates
+                video_writer.write(output_image)
                 session_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 self.coordinates_data.append([session, session_time, fish_x, fish_y])
-                #print(self.coordinates_data)
                 exp_instance.save_coordinates_csv(save_coordinates)
-                
+
                 previous_pattern = " "
                 current_pattern = " "
                 consecutive_same_pattern_count=0
                 
-                #Random Choice
+                #Random display of stimulus on the screen
                 if random.choice([True, False]):
                     left_screen_image = self.correct_stimulus
                     right_screen_image = self.incorrect_stimulus
@@ -193,7 +173,7 @@ class Cichlids_Train_Exp:
                     right_screen_image = self.correct_stimulus
                     current_pattern = "Incorrect"
                     
-                #Ensures same pattern is not repeated more than thrice
+                #Ensures pattern of correct/incorrect stimulus on-screen is not repeated more than thrice
                 if current_pattern == previous_pattern:
                     consecutive_same_pattern_count+=1
                     print("consecutive_same_pattern:", consecutive_same_pattern)
@@ -204,7 +184,8 @@ class Cichlids_Train_Exp:
                     consecutive_same_pattern_count = 0
                     
                 previous_pattern = current_pattern
-                
+
+                #Fish is in start area
                 if exp_instance.is_fish_in_ROI(fish_x, fish_y, start_x1, start_y1, start_x2, start_y2) == True:
                     exp_instance.displayn(left_screen_image, "Left_Screen", int(window_width/2), window_height, left_window_pos)
                     exp_instance.displayn(right_screen_image, "Right_Screen", int(window_width/2), window_height, right_window_pos)
@@ -212,7 +193,8 @@ class Cichlids_Train_Exp:
                         trial_count+=1
                         trigger_count+=1
                         food_drop_count = 0
-                        
+
+                #Fish is in region of choice
                 if exp_instance.is_fish_in_ROI(fish_x, fish_y, self.ROC_x1, self.ROC1_y1, self.ROC1_x2, self.ROC1_y2) == True:
                     trigger_count=0
                     response_count+=1
@@ -236,13 +218,12 @@ class Cichlids_Train_Exp:
                             if time.time() - fish_entry_ROC > self.display_neutral_after_time:
                                 self.displayn(self.neutral_image, "Left_Screen", int(window_width/2), window_height, left_window_pos)
                                 self.displayn(self.neutral_image, "Right_Screen", int(window_width/2), window_height, right_window_pos)
-
                                 if food_drop_count<=0:
                                     self.motor_drop(motor_right)
                                     success_count+=1
-                                    food_drop_count+=1
-                                    
+                                    food_drop_count+=1              
             break
+          
         print("Time:",time.time() - session_start_time)
         print("Trial_count:", trial_count)
         print("Maximum time or trial count reached")
@@ -252,26 +233,18 @@ class Cichlids_Train_Exp:
         print(learning_success)
         return trial_count, response_rate, learning_success
                                     
-                    
-    def run_train_exp(self):
-        
+    #Executes the training routine                
+    def run_train_exp(self): 
         session = 0
         consecutive_successful_sessions = 0
         consecutive_unfit_sessions = 0
         
         for day in range(self.max_training_days):
-            
-            
             while session <= self.max_sessions_per_day:
-                
                 date_session = date.today()
-                print('****')
                 save_recording = "T" + "_" + str(self.tank_ID)  + "_" + str(date_session) + ".avi"
-                
                 save_coordinates = "T" + "_" + str(self.tank_ID)  +  "_" + str(date_session) + "_coords.csv"
-                
                 save_training_details = "T" + "_" + str(self.tank_ID) + "_train_info.csv"
-                
                 
                 output_image, fish_x, fish_y = exp_instance.track_fish(session, save_recording)
                 #print(fish_x, fish_y)
@@ -279,9 +252,9 @@ class Cichlids_Train_Exp:
                 self.coordinates_data.append([session, session_time, fish_x, fish_y])
                 #print(self.coordinates_data)
                 exp_instance.save_coordinates_csv(save_coordinates)
-                
+
+                #Fish comes into start area for the first time and the training routine starts 
                 if exp_instance.is_fish_in_ROI(fish_x, fish_y, self.start_x1, self.start_y1, self.start_x2, self.start_y2) == True:
-                    
                     response_rate, learning_success, trial_count = self.run_session(session, save_recording, save_coordinates)
                     print("Session Ended")
                     time.sleep(self.session_interval)
@@ -318,10 +291,6 @@ class Cichlids_Train_Exp:
                 else:
                     continue
                 
-                
-               
-                    
-
                 
 experiment1 = Cichlids_Train_Exp(
     tank_ID = 1,
